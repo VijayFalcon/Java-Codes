c7a089105398f3c428303ca7ba9c2a7efaa93534
InfamousFalcon <83410745+VijayFalcon@users.noreply.github.com>
2024-07-23 17:02:23 +0530

    Create MathClass.java

---
+++ MathClass.java
@@ -0,0 +1,12 @@
+package MathClass;
+
+public class Main {
+	public static void main(String args[])
+	{
+		System.out.println(Math.min(6,9));
+		System.out.println(Math.max(6,9));
+		System.out.println(Math.sqrt(2));
+		System.out.println(Math.abs(-49));
+		System.out.println(Math.random());
+	}
+}
