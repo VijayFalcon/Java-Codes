2eab61e95b8412812894eda181633de0348bdd5d
InfamousFalcon <83410745+VijayFalcon@users.noreply.github.com>
2024-07-23 18:37:37 +0530

    Create Boolean.java

---
+++ Boolean.java
@@ -0,0 +1,11 @@
+package Boolean;
+
+public class Main {
+	public static void main(String[] args)
+	{
+		boolean isJavaFun = true;
+		boolean isFishTasty = false;
+		System.out.println(isJavaFun);
+		System.out.println(isFishTasty);
+	}
+}
