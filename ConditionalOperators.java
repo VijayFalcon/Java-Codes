7ee56707a7197bfb8252f5b2c2bbff8de9d3f63e
InfamousFalcon <83410745+VijayFalcon@users.noreply.github.com>
2024-07-23 18:41:43 +0530

    Create ConditionalOperators.java

---
+++ ConditionalOperators.java
@@ -0,0 +1,11 @@
+package ConditionalOperators;
+
+public class Main {
+	public static void main(String[] args)
+	{
+		int x = 10;
+		int y = 9;
+		System.out.println(x>y);
+		System.out.println(10==15);
+	}
+}
