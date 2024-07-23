7b5a6ce05d208ab1cf15766962788b03acef9f93
InfamousFalcon <83410745+VijayFalcon@users.noreply.github.com>
2024-07-23 17:01:35 +0530

    Create StringOperations.java

---
+++ StringOperations.java
@@ -0,0 +1,16 @@
+package StringOperations;
+
+public class Main {
+	public static void main(String args[])
+	{
+		String firstName = "John";
+		String lastName = "Doe";
+		System.out.println(firstName+lastName);
+		String x = "10";
+		int y = 20;
+		String z = x+y;
+		System.out.println(z);
+		String txt = "We are the so-called \"Vikings\" from the north.";
+		System.out.println(txt);
+	}
+}
