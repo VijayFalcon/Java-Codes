b0ce5234665ec2a3cecdc99cbd847349d90ceadb
InfamousFalcon <83410745+VijayFalcon@users.noreply.github.com>
2024-07-23 19:13:46 +0530

    Create forEach.java

---
+++ forEach.java
@@ -0,0 +1,16 @@
+package forEach;
+
+public class forEach {
+public static void main(String[] args)
+{
+	for (int i = 0;i<5;i++)
+	{
+		System.out.println(i);
+	}
+	String[] cars = {"Volvo","BMW","Ford","Mazda"};
+	for(String i:cars)
+	{
+		System.out.println(i);
+	}
+}
+}
