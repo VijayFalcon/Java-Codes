d8b64c66f5c8406294db4bfc52b96240401aaebc
InfamousFalcon <83410745+VijayFalcon@users.noreply.github.com>
2024-07-30 12:57:20 +0530

    Create StringBuilderCapacity.java

---
+++ StringBuilderCapacity.java
@@ -0,0 +1,17 @@
+//Same as string buffer.
+public class StringBuilderCapacity
+{
+    public static void main(String[] args)
+    {
+        StringBuilder sb = new StringBuilder();
+        System.out.println("Default capacity: "+sb.capacity());
+        sb.append("Hello");
+        System.out.println("Capacity after appending: "+sb.capacity());
+        sb.append("Java is my favourite language");
+        System.out.println("Capacity after appending again: "+sb.capacity()); //(16*2)+2 = 34
+        sb.ensureCapacity(10);//no change
+        System.out.println("Capacity after ensuring capacity: "+sb.capacity());
+        sb.ensureCapacity(50);//(34*2)+2 = 70
+        System.out.println("Capacity after ensuring capacity again: "+sb.capacity());
+    }
+}
