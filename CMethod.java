c94fddbb80591f40dce658c1c20cd0705f62d296
InfamousFalcon <83410745+VijayFalcon@users.noreply.github.com>
2024-08-08 12:54:45 +0530

    Create CMethod.java

---
+++ CMethod.java
@@ -0,0 +1,10 @@
+public class CMethod {
+    static void myMethod()
+    {
+        System.out.println("Hello World!");
+    }
+    public static void main(String[] args) {
+        CMethod c = new CMethod();
+        c.myMethod();
+    } 
+}
