cc910b0894f372c014a502ee65cda714b355c39f
InfamousFalcon <83410745+VijayFalcon@users.noreply.github.com>
2024-08-08 12:51:52 +0530

    Create MulAttributes.java

---
+++ MulAttributes.java
@@ -0,0 +1,10 @@
+public class MulAttributes {
+    String fname = "Pete";
+    String lname = "Sahut";
+    int age = 24;
+    public static void main(String[] args)
+    {
+        MulAttributes m = new MulAttributes();
+        System.out.printf("His name is: %s %s and he is %d years old.",m.fname,m.lname,m.age);
+    }   
+}
