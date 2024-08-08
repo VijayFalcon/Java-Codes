4329998acda8efd76778b0c3f138abed0afd6862
InfamousFalcon <83410745+VijayFalcon@users.noreply.github.com>
2024-08-08 13:10:36 +0530

    Create ParaC.java

---
+++ ParaC.java
@@ -0,0 +1,11 @@
+public class ParaC {
+    int x;
+    public ParaC(int x)
+    {
+        this.x = x;
+    }
+    public static void main(String[] args) {
+        ParaC p = new ParaC(105);
+        System.out.println(p.x);
+    }
+}
