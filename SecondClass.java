c3e70c064e7589fcb5ca9442a556f28bbe97e43d
InfamousFalcon <83410745+VijayFalcon@users.noreply.github.com>
2024-08-08 12:33:24 +0530

    Create SecondClass.java

---
+++ SecondClass.java
@@ -0,0 +1,7 @@
+public class SecondClass {
+    public static void main(String[] args)
+    {
+        FirstClass f = new FirstClass();
+        System.out.printf("The coolest fighter is the F-%d",f.x);
+    }
+}
