5b13f82a005c9d480c103b9c05a7dcb4766c9574
InfamousFalcon <83410745+VijayFalcon@users.noreply.github.com>
2024-08-08 13:07:33 +0530

    Create Constructors.java

---
+++ Constructors.java
@@ -0,0 +1,11 @@
+public class Constructors {
+int x;
+public Constructors()
+{
+    x = 5;
+}   
+public static void main(String[] args) {
+    Constructors c = new Constructors();
+    System.out.println(c.x);
+} 
+}
