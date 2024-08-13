4f02a5d86bc6b955c6f07a1ced352fccdca95772
InfamousFalcon <83410745+VijayFalcon@users.noreply.github.com>
2024-08-13 12:12:40 +0530

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,12 @@
+public class Person {
+    private String name;
+    //Getter
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String newName)
+    {
+        this.name = newName;
+    }   
+}
