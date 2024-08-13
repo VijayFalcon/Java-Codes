b2f403b114518115cc52b38d2a0059f9605d654a
InfamousFalcon <83410745+VijayFalcon@users.noreply.github.com>
2024-08-13 13:16:49 +0530

    Create PigIF.java

---
+++ PigIF.java
@@ -0,0 +1,10 @@
+class PigIF implements AnimalIF {
+    public void animalSound()
+    {
+        System.out.println("The pig says: wee wee");
+    }
+    public void sleep()
+    {
+        System.out.println("Zzz");
+    }
+}
