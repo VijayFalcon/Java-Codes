73435045666e55dac37c9e89d9302efaf57ea3fe
InfamousFalcon <83410745+VijayFalcon@users.noreply.github.com>
2024-08-13 12:13:02 +0530

    Create Encapsulation.java

---
+++ Encapsulation.java
@@ -0,0 +1,8 @@
+public class Encapsulation {
+    public static void main(String[] args)
+    {
+        Person p = new Person();
+        p.setName("John");
+        System.out.println(p.name);
+    }    
+}
