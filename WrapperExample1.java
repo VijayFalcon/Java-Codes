fdd3fbe98cf03f15859bb058d2df0bfa9294622c
InfamousFalcon <83410745+VijayFalcon@users.noreply.github.com>
2024-08-19 17:20:04 +0530

    Create WrapperExample1.java

---
+++ WrapperExample1.java
@@ -0,0 +1,9 @@
+package WrapperExample1;
+public class WrapperExample1 {
+	public static void main(String[] args) {
+		int a = 20;;
+		Integer intObj = a;
+		int intVal = Integer.valueOf(a);
+		System.out.println(a+" "+intObj+" "+intVal);
+	}
+}
