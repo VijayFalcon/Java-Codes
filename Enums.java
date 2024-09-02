33c154be9c3695f77c1abde6d26beff2abc5a3e3
InfamousFalcon <83410745+VijayFalcon@users.noreply.github.com>
2024-09-02 17:14:13 +0530

    Create Enums.java

---
+++ Enums.java
@@ -0,0 +1,17 @@
+package Enums;
+
+enum Level
+{
+	LOW,
+	MEDIUM,
+	HIGH
+}
+
+public class Enums
+{
+	public static void main(String[] args)
+	{
+		Level myVar = Level.MEDIUM;
+		System.out.println(myVar);
+	}
+}
